391948545fe438ec874ad4c99f2491ae296ed768
jaiv shah <jaiv.maurya@gmail.com>
2021-02-02 19:47:08 -0500

    Solve containsDuplicate and put it inside rotateArray.java file.

--- practice/src/com/company/RotateArray.java
+++ practice/src/com/company/RotateArray.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class RotateArray {
     public void reverse(int[] nums, int start, int end){
         while (start < end){
@@ -18,5 +20,13 @@ public class RotateArray {
 //        reverse(nums, 0 , nums.length-1);
          reverse(nums, 0, k-1);
          reverse(nums, k, nums.length-1);
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length -1 ; i++) {
+            if (nums[i] == nums [i+1]) return true;
+        }
+        return false;
     }
 }
