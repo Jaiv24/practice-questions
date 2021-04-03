9b12fd8981f20cddedb22997642dee58c9ab4c15
jaiv shah <jaiv.maurya@gmail.com>
2021-04-03 16:03:50 -0400

    Did some changes to singleNumber.java

--- practice/src/com/company/singleNumber.java
+++ practice/src/com/company/singleNumber.java
@@ -8,13 +8,13 @@ public class singleNumber {
     static int singlNumber(int[] nums) {
         int a=0;
         for (int i : nums){
-            a ^= i; // ^= is XOR gate representation a(XOR)0=a and a(XOR)a=0
+            a = a^i; // ^= is XOR gate representation a(XOR)0=a and a(XOR)a=0
         }
         return a;
     }
 
     public static void main(String[] args) {
-        int[] x = {4,2,1,2,1};
+        int[] x = {4,6,6,4,2,1,2,1};
         System.out.println(singlNumber(x));
     }
 }
