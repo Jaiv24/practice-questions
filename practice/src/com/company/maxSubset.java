61f9a30ccf3da00696278f72fd45c594babb97ea
Jaiv Shah <39690996+Jaiv24@users.noreply.github.com>
2021-04-20 13:27:18 -0400

    Update maxSubset.java

--- practice/src/com/company/maxSubset.java
+++ practice/src/com/company/maxSubset.java
@@ -2,14 +2,12 @@ package com.company;
 
 public class maxSubset {
     public static int maxSubsetSumNoAdjecent(int[] array){
-
         if (array.length == 0){
             return 0;
         }
         if (array.length == 1){
             return array[0];
         }
-
         int second = array[0];
         int first = Math.max(array[0], array[1]);
         for (int i=2; i<array.length; i++){
