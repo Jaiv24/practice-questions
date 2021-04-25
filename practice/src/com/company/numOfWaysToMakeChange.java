354e6d0fefbb2db9e8f2ada0385b98ed93ad86b5
jaiv shah <jaiv.maurya@gmail.com>
2021-04-25 18:13:48 -0400

    Solved number of ways to make change

---
+++ practice/src/com/company/numOfWaysToMakeChange.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class numOfWaysToMakeChange {
+    public static int numberOfWaysToMakeChange(int n, int[] denoms){
+        int[] ways = new int[n+1];
+        ways[0] = 1;
+        for (int demon:denoms){
+            for (int amount =1 ; amount<n+1; amount++ ){
+                if (demon <= amount){
+                    ways[amount] += ways[amount - demon];
+                }
+            }
+        }
+        return ways[n];
+    }
+}
+
